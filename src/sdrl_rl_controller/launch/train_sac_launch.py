66d44db505b1f2d3580bb028684326034011bd90
Chih-An Lion Lai <lionlai1989@gmail.com>
2025-11-17 02:31:25 +0100

    Add use_sim_time to rl training script

--- src/sdrl_rl_controller/launch/train_sac_launch.py
+++ src/sdrl_rl_controller/launch/train_sac_launch.py
@@ -1,17 +1,25 @@
 from launch import LaunchDescription
-from launch.actions import EmitEvent, RegisterEventHandler
+from launch.actions import DeclareLaunchArgument, EmitEvent, RegisterEventHandler
 from launch.event_handlers import OnProcessExit
 from launch.events import Shutdown
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 
 def generate_launch_description() -> LaunchDescription:
+    use_sim_time_arg = DeclareLaunchArgument(
+        "use_sim_time",
+        default_value="true",
+        description="Use simulation time",
+    )
+
     # RL training node
     rl_training_node = Node(
         package="sdrl_rl_controller",
         executable="train_sac",
         name="sac_trainer",
         output="screen",
+        parameters=[{"use_sim_time": LaunchConfiguration("use_sim_time")}],
     )
     shutdown_on_rl_exit = RegisterEventHandler(
         OnProcessExit(
@@ -19,4 +27,4 @@ def generate_launch_description() -> LaunchDescription:
             on_exit=[EmitEvent(event=Shutdown(reason="RL training finished"))],
         )
     )
-    return LaunchDescription([rl_training_node, shutdown_on_rl_exit])
+    return LaunchDescription([use_sim_time_arg, rl_training_node, shutdown_on_rl_exit])
